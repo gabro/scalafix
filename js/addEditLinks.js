ac6a15cafb6f6ddb6c2df5c7525871de19e7e62e
Gabriele Petronella <gabriele@buildo.io>
2017-10-01 22:24:12 +0200

    updated site

---
+++ js/addEditLinks.js
@@ -0,0 +1,9 @@
+const relativePath = window.location.pathname.replace('/scalafix/docs/', '').replace('.html', '.md');
+const repo = 'gabro/scalafix';
+const branch = 'microsite';
+const basePath = `https://github.com/${repo}/edit/${branch}/website/src/main/tut/docs`;
+const target = `${basePath}/${relativePath}`;
+const editLink = document.createElement('li');
+const text = 'EDIT THIS PAGE ON GITHUB'
+editLink.innerHTML = `<a href='${target}' target='_blank'><span>${text}</span></a>`
+document.querySelector('.nav .pull-right').appendChild(editLink);
