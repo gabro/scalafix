1755767356e77c0ab36538ded913f4e8b428d79e
Gabriele Petronella <gabriele@buildo.io>
2017-10-02 09:46:39 +0200

    updated site

--- js/addEditLinks.js
+++ js/addEditLinks.js
@@ -1,12 +1,19 @@
-let relativePath = window.location.pathname.replace('/scalafix/docs/', '').replace('.html', '.md');
-if (relativePath === '') {
-  relativePath = 'index.md'
+const width = window.document.documentElement.clientWidth;
+// The edit link is too large below this width and it's not useful anyway,
+// as the user is probably on a mobile device.
+if (width > 500) {
+  const baseUrl = '/scalafix';
+  const docsUrl = 'docs';
+  let relativePath = window.location.pathname.replace(`${baseUrl}/${docsUrl}/`, '').replace('.html', '.md');
+  if (relativePath === '') {
+    relativePath = 'index.md'
+  }
+  const repo = 'gabro/scalafix';
+  const branch = 'microsite';
+  const basePath = `https://github.com/${repo}/edit/${branch}/website/src/main/tut/${docsUrl}`;
+  const target = `${basePath}/${relativePath}`;
+  const editLink = document.createElement('li');
+  const text = 'EDIT THIS PAGE ON GITHUB';
+  editLink.innerHTML = `<a href='${target}' target='_blank'><span>${text}</span></a>`
+  document.querySelector('.nav .pull-right').appendChild(editLink);
 }
-const repo = 'gabro/scalafix';
-const branch = 'microsite';
-const basePath = `https://github.com/${repo}/edit/${branch}/website/src/main/tut/docs`;
-const target = `${basePath}/${relativePath}`;
-const editLink = document.createElement('li');
-const text = 'EDIT THIS PAGE ON GITHUB'
-editLink.innerHTML = `<a href='${target}' target='_blank'><span>${text}</span></a>`
-document.querySelector('.nav .pull-right').appendChild(editLink);
